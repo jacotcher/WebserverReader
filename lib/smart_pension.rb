3d762bf8bb7a045d420dcb388c72b41abb568202
James Cotcher <jacotcher1@sheffield.ac.uk>
2019-07-16 10:28:34 +0100

    Read file successfuly and tested

--- lib/smart_pension.rb
+++ lib/smart_pension.rb
@@ -3,5 +3,14 @@ end
 
 
 def read_file(filename)
+  begin
+    file_contents = File.readlines filename
+    return file_contents
+  rescue Errno::ENOENT
+    return false
+  end
 
 end
+
+puts read_file("te")
+visit_array = read_file("ARGV0")
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,14 +2,14 @@ require "smart_pension"
 
 
 describe "Reading file" do
-  context 'Correct filename is given as argument' do
+  context 'Correct filename is given as argument and file exists' do
     it "webserver.log" do
-      expect(read_file("webserver.log")).to be_true
+      expect(read_file("webserver.log")).to be_an_instance_of(Array)
     end
   end
-  context "Incorrect filename is given as argument" do
+  context "Incorrect filename is given as argument/file does not exist" do
     it "weberv.log" do
-      expect(read_file("weberv.log")).not_to be_true
+      expect(read_file("weberv.log")).to be false
     end
   end
 
